6dc113aa7c6ebefd902b356d6b83f2189154a62e
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-04 15:57:43 -0700

    Cleanup before new data point.

--- src/dev/code_coverage/ingest_coverage/ingest.js
+++ src/dev/code_coverage/ingest_coverage/ingest.js
@@ -20,33 +20,43 @@
 const { Client } = require('@elastic/elasticsearch');
 import { createFailError } from '@kbn/dev-utils';
 import chalk from 'chalk';
-import { green } from './utils'
-import { fromNullable } from './either';
-import { always } from './utils'
+import { green } from './utils';
+import { fromNullable as fN, tryCatch as tc, left, right } from './either';
+import { always as F } from './utils';
 
 const COVERAGE_INDEX = process.env.COVERAGE_INDEX || 'kibana_code_coverage';
 const TOTALS_INDEX = process.env.TOTALS_INDEX || `kibana_total_code_coverage`;
+
 const node = process.env.ES_HOST || 'http://localhost:9200';
-const redacted = redact(node);
 const client = new Client({ node });
+const redacted = redact(node);
+
+const red = color('red');
+
+const logAndSend = log => index => body =>
+  tc(async () => await client.index({ index, body }))
+    .fold(
+      e => { throw createFailError(errMsg(index, body, e))},
+      F(logSuccess(log, index, body))
+    );
+
+const logOnly = log => index => body => {
+  log.debug(green(`### Just Logging, ${red('NOT')} actually sending`));
+  logSuccess(log, index, body);
+}
+const logOrSendAndLog = process.env.NODE_ENV === 'integration_test' ? left() : right();
 
 export const ingest = log => async body => {
   const index = !body.staticSiteUrl ? TOTALS_INDEX : COVERAGE_INDEX;
 
-  if (process.env.NODE_ENV === 'integration_test') {
-    log.debug('### Just Logging, not actually sending')
-    logSuccess(log, index, body);
-  } else {
-    try {
-      await client.index({ index, body });
-      logSuccess(log, index, body);
-    } catch (e) {
-      throw createFailError(errMsg(index, body, e));
-    }
-  }
+  logOrSendAndLog
+    .fold(
+      F(logOnly(log)(index)(body)),
+      F(logAndSend(log)(index)(body))
+    );
+};
 
-}
-function logSuccess(log, index, body) {
+function logSuccess (log, index, body) {
   log.verbose(`
 ### Sent:
 ### ES HOST (redacted): ${redacted}
@@ -54,7 +64,7 @@ function logSuccess(log, index, body) {
 ${pretty(body)}
 `);
 
-  const {staticSiteUrl} = body;
+  const { staticSiteUrl } = body;
 
   log.debug(`
 ### Sent:
@@ -62,10 +72,8 @@ ${pretty(body)}
 ### staticSiteUrl: ${staticSiteUrl}
 `);
 }
-function errMsg(index, body, e) {
-  const orig = fromNullable(e.body).fold(always(''), () => `### Orig Err:\n${pretty(e.body.error)}`);
-
-  const red = color('red');
+function errMsg (index, body, e) {
+  const orig = fN(e.body).fold(F(''), () => `### Orig Err:\n${pretty(e.body.error)}`);
 
   return `
 ### ES HOST (redacted): \n\t${red(redacted)}
@@ -80,13 +88,13 @@ ${red('Perhaps the coverage data was not merged properly?\n')}
 
 }
 
-function partial(x) {
+function partial (x) {
   return x
     .split('\n')
     .splice(0, 2)
     .join('\n');
 }
-function redact(x) {
+function redact (x) {
   const url = new URL(x);
   if (url.password) {
     return `${url.protocol}//${url.host}`;
@@ -94,11 +102,11 @@ function redact(x) {
     return x;
   }
 }
-function color(whichColor) {
-  return function colorInner(x) {
+function color (whichColor) {
+  return function colorInner (x) {
     return chalk[whichColor].bgWhiteBright(x);
-  }
+  };
 }
-function pretty(x) {
+function pretty (x) {
   return JSON.stringify(x, null, 2);
 }
